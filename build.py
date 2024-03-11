b4b31d299a6e69f8df2084c540ec884692d99773
BRNMan <msbraun@cinci.rr.com>
2024-03-11 00:29:31 -0500

    Initial commit

---
+++ build.py
@@ -0,0 +1,19 @@
+import os
+from datetime import date
+import random
+
+myFile =  open("index.htm","r")
+fileText = myFile.read()
+myFile.close()
+imageDirFiles = os.listdir("./images")
+imageFiles = list(filter(lambda img: img.endswith(".jpg"), imageDirFiles))
+
+random.seed(date.today().isoformat())
+curImage = random.choice(imageFiles)
+
+fileText = fileText.replace("{#RandomImage#}", '"/images/' + curImage + '"')
+
+f = open("index.html", "w")
+f.write(fileText)
+f.close()
+
